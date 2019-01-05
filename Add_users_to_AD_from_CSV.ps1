6c924e88219cc842da37f6b35ab2cfed3ead35da
disk5464 <36386147+disk5464@users.noreply.github.com>
2019-01-05 14:27:19 -0500

    Add files via upload

---
+++ Add_users_to_AD_from_CSV.ps1
@@ -0,0 +1,9 @@
+﻿#This script imports a csv along with it's headers and then uses the data to cerate a user for each entry
+####################################################################################
+
+$AD_Csv = Import-Csv -Path 'D:\Important Files\College\Blum Project\AD_Users.csv' -Header first, last, first_and_last, email, display_name, user_name
+
+foreach ($name in $AD_Csv)
+{
+    New-ADUser -Name $name.first_and_last -Credential $name.user_name -DisplayName $name.display_name -EmailAddress $name.email -Enabled $True -PasswordNotRequired $true
+}
